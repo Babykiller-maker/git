b7a48ddf8774692c19dcabb89ebf8ed7e0d9dfcd
luckyvishu <luckyvishu7975@gmail.com>
2025-10-03 12:19:46 +0530

    createcampaigns

--- src/test/java/campaigns/CreateCampaignWithMandatoryFields.java
+++ src/test/java/campaigns/CreateCampaignWithMandatoryFields.java
@@ -3,7 +3,7 @@ package campaigns;
 public class CreateCampaignWithMandatoryFields {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello");
 
 	}
 
